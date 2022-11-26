b38c0a225fa0d48b3e2172b7be9d3af1b12fcbe5
dbstn1325 <dbstn1325@naver.com>
2022-11-26 21:34:03 +0900

    Add Atomic-design

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Page404 from "./pages/404page";
+import { Detail } from "./pages/Detail/Detail";
 import { Home } from "./pages/Home/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <RecoilRoot>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
         <Route path="/*" element={<Page404 />} />
       </Routes>
     </RecoilRoot>
