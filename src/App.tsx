d51750912641924b707d676b9956812e55ace74a
dbstn1325 <dbstn1325@naver.com>
2022-11-26 21:06:15 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Page404 from "./pages/404page";
+import { Home } from "./pages/Home/Home";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </RecoilRoot>
+  );
+}
+
+export default App;
