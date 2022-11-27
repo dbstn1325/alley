832341236c9f62e99b9dea95e4473507289d904f
soyuly <soyul8147@gmail.com>
2022-11-27 11:38:50 +0900

    [bug] 폰트 버그 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { GlobalStyle } from "styles/global/global_style";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </>
