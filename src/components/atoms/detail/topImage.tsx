4765849a0b0423aac65642b27ecaf47af607fa3d
dbstn1325 <dbstn1325@naver.com>
2022-11-27 11:41:18 +0900

    detail Page Styled-Component Init

---
+++ src/components/atoms/detail/topImage.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TopImage = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 15px;
+  background-color: black;
+`;
+
+export default TopImage;
