4331eccb892ecb2a046c6aff633c832c956f4ec2
dbstn1325 <dbstn1325@naver.com>
2022-11-27 20:40:10 +0900

    Detail Page 100%

--- src/components/atoms/detail/fontStyle.tsx
+++ src/components/atoms/detail/fontStyle.tsx
@@ -6,11 +6,13 @@ interface FontStyleProps {
   font_family?: string;
   padding_top?: number;
   color?: string;
+  line_height?: number;
 }
 
 export const FontStyle = styled.div<FontStyleProps>`
   font-size: ${(props) => (props ? props.font_size : 10)}rem;
-  font-family: ${(props) => props.font_family};
+  font-family: ${(props) => (props ? props.font_family : "PretendardRegular")};
   padding-top: ${(props) => props.padding_top}rem;
   color: ${(props) => (props ? props.color : "#3B3B3B")};
+  line-height: ${(props) => (props ? props.line_height : null)}rem;
 `;
