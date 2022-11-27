b8b4b073ed821797feefcbc87bee41cd2b43a8c4
soyuly <soyul8147@gmail.com>
2022-11-28 02:02:26 +0900

    [fix] link수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,10 @@
-import Address from "components/atoms/home/address";
-import Area from "components/atoms/home/Area";
-import { AddressStyle } from "components/atoms/home/style";
-
-import SubTitle from "components/atoms/home/sub_title";
-import Title from "components/atoms/home/title";
-import NavButton from "components/atoms/nav/nav_button";
-import Distance from "components/molecules/home/distance";
-import ItemInfo from "components/molecules/home/item_info";
-import NavBar from "components/molecules/nav/navbar";
-import AlleyItem from "components/organisms/home/alley_item";
 import HomeTemplate from "components/templates/home/home";
+import { useEffect } from "react";
 
 export const Home = () => {
+  useEffect(() => {
+    document.title = "Alley";
+  }, []);
   return (
     <>
       <HomeTemplate />
